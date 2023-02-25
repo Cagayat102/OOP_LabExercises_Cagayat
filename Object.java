303e672f58f88ab9c026a33360897a27b7ca5b4b
Cagayat102 <126264428+Cagayat102@users.noreply.github.com>
2023-02-24 22:01:55 -0800

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package object;
+import java.util.*;
+
+public class Object {
+    private double length;
+    private double width;
+    
+    
+        private Object (double length, double width){
+            this.length = 0;
+            this.width = 0;
+            
+        }
+    Scanner rectangle = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+  {
+    double width;
+    System.out.println("Enter width. ");
+    width = keyboard.nextDouble();
+    System.out.println("The width is " + width);
+  }
+  
+  {
+   double length = 0;
+   double width = 0;
+   double area = length * width;
+
+   System.out.println("The area is: " + area);
+    }
+    }
+}
+    
+
